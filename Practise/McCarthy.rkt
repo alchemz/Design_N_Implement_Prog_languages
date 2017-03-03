557cf4c70089a698e1663df50245b4bb3ae7764e
xianling zhang <alchemxz@gmail.com>
2017-03-02 21:25:59 -0500

    Create McCarthy.rkt

---
+++ Practise/McCarthy.rkt
@@ -0,0 +1,9 @@
+(define mac
+           (lambda (n)
+                   (if (> n 100)
+                          (- n 10)
+           
+                           
+                   (if(<= n 100)
+                           (ack (ack (+ n 11))))
+                           )))
